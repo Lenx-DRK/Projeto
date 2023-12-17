d06d88a1ff3a04b567e1e6cf5f234a89678248bc
Alex Lopes <lex.lopes.ti@gmail.com>
2023-12-17 15:27:32 -0300

    Tela inciial com botão

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,112 @@
-import '.App.css';
+//import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react'
 
-function App(){
-    return(
-        <div>
-            <H1>Estado de Origem</H1>
-            <label>Estado de Origem</label>
-            <select name='Estado de Origem'>
-                
-            </select>
+function App() {
+
+  const [selectOrig] = useState(9);
+  const [selectDest] = useState(2);
+  const enviarEstados = e =>{
+    e.preventDefault();
+    alert("Botão")
+  }
+
+  return (
+    <div className="cabecalhoOrig">
+      <h1>Estado de Origem</h1>
+
+      <form onSubmit={enviarEstados}>
+
+        <div className="estOrig">
+
+
+
+          <label>Estado de Origem: </label>
+
+          <select name="selectOrig" value={selectOrig}>
+
+            <option value="">ESTADO DE ORIGEM</option>
+            <option value="1">Acre - AC</option>,
+            <option value="2">Alagoas - AL</option>
+            <option value="3"> Amapá - AP </option>,
+            <option value="4"> Amazonas - AM </option>,
+            <option value="5"> Bahia - BA </option>,
+            <option value="6"> Ceará CE </option>,
+            <option value="7"> Distrito Federal - DF </option>,
+            <option value="8"> Espírito Santo - ES </option>,
+            <option value="9"> Goiás - GO </option>,
+            <option value="10"> Maranhão - MA </option>,
+            <option value="11"> Mato Grosso - MT </option>,
+            <option value="12"> Mato Grosso do Sul - MS </option>,
+            <option value="13"> Minas Gerais - MG </option>,
+            <option value="14"> Pará - PA </option>,
+            <option value="15"> Paraíba - PB </option>,
+            <option value="16"> Paraná - PR </option>,
+            <option value="17"> Pernambuco - PE </option>,
+            <option value="18"> Piauí - PI </option>,
+            <option value="19"> Rio de Janeiro - RJ </option>,
+            <option value="20"> Rio Grande do Norte - RN</option>,
+            <option value="21"> Rio Grande do Sul - RS</option>,
+            <option value="22"> Rondônia - RO </option>,
+            <option value="23"> Roraima - RR </option>,
+            <option value="24"> Santa Catarina - SC </option>,
+            <option value="25"> São Paulo - SP </option>,
+            <option value="26"> Sergipe - SE </option>,
+            <option value="27"> Tocantins - TO </option>
+
+          </select>
+
+
+          <div className="cabecalhoDest">
+            <h1>Estado de Destino</h1>
+
+            <div className="estDest">
+
+              <label>Estado de Destino: </label>
+
+              <select name="selectDest" value={selectDest}>
+
+                <option value="">ESTADO DE ORIGEM</option>
+                <option value="1">Acre - AC</option>,
+                <option value="2">Alagoas - AL</option>
+                <option value="3"> Amapá - AP </option>,
+                <option value="4"> Amazonas - AM </option>,
+                <option value="5"> Bahia - BA </option>,
+                <option value="6"> Ceará CE </option>,
+                <option value="7"> Distrito Federal - DF </option>,
+                <option value="8"> Espírito Santo - ES </option>,
+                <option value="9"> Goiás - GO </option>,
+                <option value="10"> Maranhão - MA </option>,
+                <option value="11"> Mato Grosso - MT </option>,
+                <option value="12"> Mato Grosso do Sul - MS </option>,
+                <option value="13"> Minas Gerais - MG </option>,
+                <option value="14"> Pará - PA </option>,
+                <option value="15"> Paraíba - PB </option>,
+                <option value="16"> Paraná - PR </option>,
+                <option value="17"> Pernambuco - PE </option>,
+                <option value="18"> Piauí - PI </option>,
+                <option value="19"> Rio de Janeiro - RJ </option>,
+                <option value="20"> Rio Grande do Norte - RN</option>,
+                <option value="21"> Rio Grande do Sul - RS</option>,
+                <option value="22"> Rondônia - RO </option>,
+                <option value="23"> Roraima - RR </option>,
+                <option value="24"> Santa Catarina - SC </option>,
+                <option value="25"> São Paulo - SP </option>,
+                <option value="26"> Sergipe - SE </option>,
+                <option value="27"> Tocantins - TO </option>
+
+              </select><br/><br/>
+            </div>
+          </div>
         </div>
-    )
+        <button type="submit">
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
 }
+
+export default App;
+
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,83 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-//import Select from 'react-select';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-
-const estOrig = [
-    { value: 'AC', label: 'Acre (AC)' },
-    { value: 'AL', label: 'Alagoas (AL)' },
-    { value: 'AP', label: 'Amapá (AP)' },
-    { value: 'AM', label: 'Amazonas (AM)' },
-    { value: 'BA', label: 'Bahia (BA)' },
-    { value: 'CE', label: 'Ceará (CE)' },
-    { value: 'DF', label: 'Distrito Federal (DF)' },
-    { value: 'ES', label: 'Espírito Santo (ES)' },
-    { value: 'GO', label: 'Goiás (GO)' },
-    { value: 'MA', label: 'Maranhão (MA)' },
-    { value: 'MT', label: 'Mato Grosso (MT)' },
-    { value: 'MS', label: 'Mato Grosso do Sul (MS)' },
-    { value: 'MG', label: 'Minas Gerais (MG)' },
-    { value: 'PA', label: 'Pará (PA)' },
-    { value: 'PB', label: 'Paraíba (PB)' },
-    { value: 'PR', label: 'Paraná (PR)' },
-    { value: 'PE', label: 'Pernambuco (PE)' },
-    { value: 'PI', label: 'Piauí (PI)' },
-    { value: 'RJ', label: 'Rio de Janeiro (RJ)' },
-    { value: 'RN', label: 'Rio Grande do Norte (RN)' },
-    { value: 'RS', label: 'Rio Grande do Sul (RS)' },
-    { value: 'RO', label: 'Rondônia (RO)' },
-    { value: 'RR', label: 'Roraima (RR)' },
-    { value: 'SC', label: 'Santa Catarina (SC)' },
-    { value: 'SP', label: 'São Paulo (SP)' },
-    { value: 'SE', label: 'Sergipe (SE)' },
-    { value: 'TO', label: 'Tocantins (TO)' }
-]
-
-const estDest = [
-    { value: 'AC', label: 'Acre (AC)' },
-    { value: 'AL', label: 'Alagoas (AL)' },
-    { value: 'AP', label: 'Amapá (AP)' },
-    { value: 'AM', label: 'Amazonas (AM)' },
-    { value: 'BA', label: 'Bahia (BA)' },
-    { value: 'CE', label: 'Ceará (CE)' },
-    { value: 'DF', label: 'Distrito Federal (DF)' },
-    { value: 'ES', label: 'Espírito Santo (ES)' },
-    { value: 'GO', label: 'Goiás (GO)' },
-    { value: 'MA', label: 'Maranhão (MA)' },
-    { value: 'MT', label: 'Mato Grosso (MT)' },
-    { value: 'MS', label: 'Mato Grosso do Sul (MS)' },
-    { value: 'MG', label: 'Minas Gerais (MG)' },
-    { value: 'PA', label: 'Pará (PA)' },
-    { value: 'PB', label: 'Paraíba (PB)' },
-    { value: 'PR', label: 'Paraná (PR)' },
-    { value: 'PE', label: 'Pernambuco (PE)' },
-    { value: 'PI', label: 'Piauí (PI)' },
-    { value: 'RJ', label: 'Rio de Janeiro (RJ)' },
-    { value: 'RN', label: 'Rio Grande do Norte (RN)' },
-    { value: 'RS', label: 'Rio Grande do Sul (RS)' },
-    { value: 'RO', label: 'Rondônia (RO)' },
-    { value: 'RR', label: 'Roraima (RR)' },
-    { value: 'SC', label: 'Santa Catarina (SC)' },
-    { value: 'SP', label: 'São Paulo (SP)' },
-    { value: 'SE', label: 'Sergipe (SE)' },
-    { value: 'TO', label: 'Tocantins (TO)' }
-]
-
-
-export default function listEstOrig1 () {
-    return (
-    <label>
-        Informe o estado de Origem:
-        <select name="teste">
-            <option value="selecione"> "teste"</option>
-
-        </select>
-    </label>
-    )
-}
-
-ReactDOM.render(
-    <listEstOrig1 />,
-    document.getElementById('root')
-    );
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
