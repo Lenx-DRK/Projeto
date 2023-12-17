6b0f44d0658897dc69aa5bec75aed485fbdcc80d
Alex Lopes <lex.lopes.ti@gmail.com>
2023-12-17 16:04:31 -0300

    Recuperando os valores selecionados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,12 @@ import React, { useState } from 'react'
 
 function App() {
 
-  const [selectOrig] = useState(9);
-  const [selectDest] = useState(2);
-  const enviarEstados = e =>{
+  const [selectOrig, setEstadoOrig] = useState();
+  const [selectDest, setEstadoDest] = useState();
+
+  const enviarEstados = e => {
     e.preventDefault();
-    alert("Botão")
+    alert("Os estados selecionados são:" + selectOrig + selectDest);
   }
 
   return (
@@ -23,36 +24,36 @@ function App() {
 
           <label>Estado de Origem: </label>
 
-          <select name="selectOrig" value={selectOrig}>
+          <select name="selectOrig" value={selectOrig} onChange={valorOrig => setEstadoOrig(valorOrig.target.value)}>
 
             <option value="">ESTADO DE ORIGEM</option>
-            <option value="1">Acre - AC</option>,
-            <option value="2">Alagoas - AL</option>
-            <option value="3"> Amapá - AP </option>,
-            <option value="4"> Amazonas - AM </option>,
-            <option value="5"> Bahia - BA </option>,
-            <option value="6"> Ceará CE </option>,
-            <option value="7"> Distrito Federal - DF </option>,
-            <option value="8"> Espírito Santo - ES </option>,
-            <option value="9"> Goiás - GO </option>,
-            <option value="10"> Maranhão - MA </option>,
-            <option value="11"> Mato Grosso - MT </option>,
-            <option value="12"> Mato Grosso do Sul - MS </option>,
-            <option value="13"> Minas Gerais - MG </option>,
-            <option value="14"> Pará - PA </option>,
-            <option value="15"> Paraíba - PB </option>,
-            <option value="16"> Paraná - PR </option>,
-            <option value="17"> Pernambuco - PE </option>,
-            <option value="18"> Piauí - PI </option>,
-            <option value="19"> Rio de Janeiro - RJ </option>,
-            <option value="20"> Rio Grande do Norte - RN</option>,
-            <option value="21"> Rio Grande do Sul - RS</option>,
-            <option value="22"> Rondônia - RO </option>,
-            <option value="23"> Roraima - RR </option>,
-            <option value="24"> Santa Catarina - SC </option>,
-            <option value="25"> São Paulo - SP </option>,
-            <option value="26"> Sergipe - SE </option>,
-            <option value="27"> Tocantins - TO </option>
+            <option value="Acre" label='Acre - (AC)'></option>,
+            <option value="Alagoas" label='Alagoas - AL'></option>
+            <option value="Amapá"> Amapá - AP </option>,
+            <option value="Amazonas"> Amazonas - AM </option>,
+            <option value="Bahia"> Bahia - BA </option>,
+            <option value="Ceará"> Ceará CE </option>,
+            <option value="Distrito Federal"> Distrito Federal - DF </option>,
+            <option value="Espírito Santo"> Espírito Santo - ES </option>,
+            <option value="Goiás"> Goiás - GO </option>,
+            <option value="Maranhão"> Maranhão - MA </option>,
+            <option value="Mato Grosso"> Mato Grosso - MT </option>,
+            <option value="Mato Grosso do Sul"> Mato Grosso do Sul - MS </option>,
+            <option value="Minas Gerais"> Minas Gerais - MG </option>,
+            <option value="Pará"> Pará - PA </option>,
+            <option value="Paraíba"> Paraíba - PB </option>,
+            <option value="Paraná"> Paraná - PR </option>,
+            <option value="Pernambuco"> Pernambuco - PE </option>,
+            <option value="Piauí"> Piauí - PI </option>,
+            <option value="Rio de Janeiro "> Rio de Janeiro - RJ </option>,
+            <option value="Rio Grande do Norte"> Rio Grande do Norte - RN</option>,
+            <option value="io Grande do Sul"> Rio Grande do Sul - RS</option>,
+            <option value="Rondônia"> Rondônia - RO </option>,
+            <option value="Roraima "> Roraima - RR </option>,
+            <option value="Santa Catarina"> Santa Catarina - SC </option>,
+            <option value="São Paulo"> São Paulo - SP </option>,
+            <option value="Sergipe"> Sergipe - SE </option>,
+            <option value="Tocantins"> Tocantins - TO </option>
 
           </select>
 
@@ -64,38 +65,38 @@ function App() {
 
               <label>Estado de Destino: </label>
 
-              <select name="selectDest" value={selectDest}>
+              <select name="selectDest" value={selectDest} onChange={valorDest => setEstadoDest(valorDest.target.value)}>
 
-                <option value="">ESTADO DE ORIGEM</option>
-                <option value="1">Acre - AC</option>,
-                <option value="2">Alagoas - AL</option>
-                <option value="3"> Amapá - AP </option>,
-                <option value="4"> Amazonas - AM </option>,
-                <option value="5"> Bahia - BA </option>,
-                <option value="6"> Ceará CE </option>,
-                <option value="7"> Distrito Federal - DF </option>,
-                <option value="8"> Espírito Santo - ES </option>,
-                <option value="9"> Goiás - GO </option>,
-                <option value="10"> Maranhão - MA </option>,
-                <option value="11"> Mato Grosso - MT </option>,
-                <option value="12"> Mato Grosso do Sul - MS </option>,
-                <option value="13"> Minas Gerais - MG </option>,
-                <option value="14"> Pará - PA </option>,
-                <option value="15"> Paraíba - PB </option>,
-                <option value="16"> Paraná - PR </option>,
-                <option value="17"> Pernambuco - PE </option>,
-                <option value="18"> Piauí - PI </option>,
-                <option value="19"> Rio de Janeiro - RJ </option>,
-                <option value="20"> Rio Grande do Norte - RN</option>,
-                <option value="21"> Rio Grande do Sul - RS</option>,
-                <option value="22"> Rondônia - RO </option>,
-                <option value="23"> Roraima - RR </option>,
-                <option value="24"> Santa Catarina - SC </option>,
-                <option value="25"> São Paulo - SP </option>,
-                <option value="26"> Sergipe - SE </option>,
-                <option value="27"> Tocantins - TO </option>
+                <option value="">ESTADO DE DESTINO</option>
+                <option value="Acre" label='Acre - (AC)'></option>,
+                <option value="Alagoas" label='Alagoas - AL'></option>
+                <option value="Amapá"> Amapá - AP </option>,
+                <option value="Amazonas"> Amazonas - AM </option>,
+                <option value="Bahia"> Bahia - BA </option>,
+                <option value="Ceará"> Ceará CE </option>,
+                <option value="Distrito Federal"> Distrito Federal - DF </option>,
+                <option value="Espírito Santo"> Espírito Santo - ES </option>,
+                <option value="Goiás"> Goiás - GO </option>,
+                <option value="Maranhão"> Maranhão - MA </option>,
+                <option value="Mato Grosso"> Mato Grosso - MT </option>,
+                <option value="Mato Grosso do Sul"> Mato Grosso do Sul - MS </option>,
+                <option value="Minas Gerais"> Minas Gerais - MG </option>,
+                <option value="Pará"> Pará - PA </option>,
+                <option value="Paraíba"> Paraíba - PB </option>,
+                <option value="Paraná"> Paraná - PR </option>,
+                <option value="Pernambuco"> Pernambuco - PE </option>,
+                <option value="Piauí"> Piauí - PI </option>,
+                <option value="Rio de Janeiro "> Rio de Janeiro - RJ </option>,
+                <option value="Rio Grande do Norte"> Rio Grande do Norte - RN</option>,
+                <option value="io Grande do Sul"> Rio Grande do Sul - RS</option>,
+                <option value="Rondônia"> Rondônia - RO </option>,
+                <option value="Roraima "> Roraima - RR </option>,
+                <option value="Santa Catarina"> Santa Catarina - SC </option>,
+                <option value="São Paulo"> São Paulo - SP </option>,
+                <option value="Sergipe"> Sergipe - SE </option>,
+                <option value="Tocantins"> Tocantins - TO </option>
 
-              </select><br/><br/>
+              </select><br /><br />
             </div>
           </div>
         </div>
