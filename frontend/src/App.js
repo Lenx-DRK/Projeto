a73a0b0f5c2fc67dd4c3ddf6382c2786e8e74d1e
Alex Lopes <lex.lopes.ti@gmail.com>
2023-12-18 02:15:03 -0300

    concluindo o textarea

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,12 +24,12 @@ function App() {
     console.log(valor.selectDest);
     axios.post('http://localhost:4000/fiscalGPT', {
       prompt: "Qual a formula do diferenical de aliquotas entre os estados de"
-      +" "
-      +valor.selectOrig
-      +" "
-      +"e"
-      +" "
-      +valor.selectDest
+        + " "
+        + valor.selectOrig
+        + " "
+        + "e"
+        + " "
+        + valor.selectDest
     })
       .then(response => {
         console.log(response);
@@ -49,96 +49,98 @@ function App() {
 
       <form onSubmit={enviarEstados}>
 
-        <div className="estOrig">
+        <div class="form-floating">
+          {/* <label>Estado de Origem: </label> */}
 
+          <select 
+          class="form-select"
+          name="selectOrig"
+          id="eO" 
+          value={selectOrig} 
+          onChange={valorOrig => setEstadoOrig(valorOrig.target.value)}>
 
-
-          <label>Estado de Origem: </label>
-
-          <select name="selectOrig" value={selectOrig} onChange={valorOrig => setEstadoOrig(valorOrig.target.value)}>
-
-            <option value="">ESTADO DE ORIGEM</option>
+            <option selected></option>
             <option value="Acre" label='Acre - (AC)'></option>,
-            <option value="Alagoas" label='Alagoas - AL'></option>
-            <option value="Amapá"> Amapá - AP </option>,
-            <option value="Amazonas"> Amazonas - AM </option>,
-            <option value="Bahia"> Bahia - BA </option>,
-            <option value="Ceará"> Ceará - CE </option>,
-            <option value="Distrito Federal"> Distrito Federal - DF </option>,
-            <option value="Espírito Santo"> Espírito Santo - ES </option>,
-            <option value="Goiás"> Goiás - GO </option>,
-            <option value="Maranhão"> Maranhão - MA </option>,
-            <option value="Mato Grosso"> Mato Grosso - MT </option>,
-            <option value="Mato Grosso do Sul"> Mato Grosso do Sul - MS </option>,
-            <option value="Minas Gerais"> Minas Gerais - MG </option>,
-            <option value="Pará"> Pará - PA </option>,
-            <option value="Paraíba"> Paraíba - PB </option>,
-            <option value="Paraná"> Paraná - PR </option>,
-            <option value="Pernambuco"> Pernambuco - PE </option>,
-            <option value="Piauí"> Piauí - PI </option>,
-            <option value="Rio de Janeiro "> Rio de Janeiro - RJ </option>,
-            <option value="Rio Grande do Norte"> Rio Grande do Norte - RN</option>,
-            <option value="Rio Grande do Sul"> Rio Grande do Sul - RS</option>,
-            <option value="Rondônia"> Rondônia - RO </option>,
-            <option value="Roraima "> Roraima - RR </option>,
-            <option value="Santa Catarina"> Santa Catarina - SC </option>,
-            <option value="São Paulo"> São Paulo - SP </option>,
-            <option value="Sergipe"> Sergipe - SE </option>,
-            <option value="Tocantins"> Tocantins - TO </option>
+            <option value="Alagoas" label='Alagoas - (AL)'></option>
+            <option value="Amapá" label='Amapá - (AP)'> </option>,
+            <option value="Amazonas" label='Amazonas - (AM) '> </option>,
+            <option value="Bahia" label='Bahia - (BA)'> </option>,
+            <option value="Ceará" label='Ceará - (CE)'> </option>,
+            <option value="Distrito Federal" label='Distrito Federal - (DF)'></option>,
+            <option value="Espírito Santo" label='Espírito Santo - (ES)'></option>,
+            <option value="Goiás" label='Goiás - (GO)'></option>,
+            <option value="Maranhão" label='Maranhão - (MA)'>  </option>,
+            <option value="Mato Grosso" label='Mato Grosso - (MT)'> </option>,
+            <option value="Mato Grosso do Sul" label='Mato Grosso do Sul - (MS)'></option>,
+            <option value="Minas Gerais" label='Minas Gerais - (MG)'> </option>,
+            <option value="Pará" label='Pará - (PA)'></option>,
+            <option value="Paraíba" label='Paraíba - (PB)'> </option>,
+            <option value="Paraná" label='Paraná - (PR)'></option>,
+            <option value="Pernambuco" label='Pernambuco - (PE)'></option>,
+            <option value="Piauí" label='Piauí - (PI)'></option>,
+            <option value="Rio de Janeiro " label='Rio de Janeiro - (RJ)'> </option>,
+            <option value="Rio Grande do Norte" label='Rio Grande do Norte - (RN)'> </option>,
+            <option value="Rio Grande do Sul" label='Rio Grande do Sul - (RS)'> </option>,
+            <option value="Rondônia" label='Rondônia - (RO)'> </option>,
+            <option value="Roraima " label='Roraima - (RR)'>  </option>,
+            <option value="Santa Catarina" label='Santa Catarina - (SC)'>  </option>,
+            <option value="São Paulo" label='São Paulo - (SP)'>  </option>,
+            <option value="Sergipe" label='Sergipe - (SE) '> </option>,
+            <option value="Tocantins" label='Tocantins - (TO) '> </option>
 
           </select>
-
-
+          <label for="eO">Selecione o Estado de Origem</label>
+          <br /><br />
           <div className="cabecalhoDest">
             <h1>Estado de Destino</h1>
 
-            <div className="estDest">
+            <div class="form-floating">
+              {/* <label>Estado de Destino: </label> */}
+              <select class="form-select" id='eS' aria-label='Exemplo'
+               name="selectDest" 
+               value={selectDest} 
+               onChange={valorDest => setEstadoDest(valorDest.target.value)}>
 
-              <label>Estado de Destino: </label>
-
-              <select name="selectDest" value={selectDest} onChange={valorDest => setEstadoDest(valorDest.target.value)}>
-
-                <option value="">ESTADO DE DESTINO</option>
+                <option selected></option>
                 <option value="Acre" label='Acre - (AC)'></option>,
-                <option value="Alagoas" label='Alagoas - AL'></option>
-                <option value="Amapá"> Amapá - AP </option>,
-                <option value="Amazonas"> Amazonas - AM </option>,
-                <option value="Bahia"> Bahia - BA </option>,
-                <option value="Ceará"> Ceará CE </option>,
-                <option value="Distrito Federal"> Distrito Federal - DF </option>,
-                <option value="Espírito Santo"> Espírito Santo - ES </option>,
-                <option value="Goiás"> Goiás - GO </option>,
-                <option value="Maranhão"> Maranhão - MA </option>,
-                <option value="Mato Grosso"> Mato Grosso - MT </option>,
-                <option value="Mato Grosso do Sul"> Mato Grosso do Sul - MS </option>,
-                <option value="Minas Gerais"> Minas Gerais - MG </option>,
-                <option value="Pará"> Pará - PA </option>,
-                <option value="Paraíba"> Paraíba - PB </option>,
-                <option value="Paraná"> Paraná - PR </option>,
-                <option value="Pernambuco"> Pernambuco - PE </option>,
-                <option value="Piauí"> Piauí - PI </option>,
-                <option value="Rio de Janeiro "> Rio de Janeiro - RJ </option>,
-                <option value="Rio Grande do Norte"> Rio Grande do Norte - RN</option>,
-                <option value="io Grande do Sul"> Rio Grande do Sul - RS</option>,
-                <option value="Rondônia"> Rondônia - RO </option>,
-                <option value="Roraima "> Roraima - RR </option>,
-                <option value="Santa Catarina"> Santa Catarina - SC </option>,
-                <option value="São Paulo"> São Paulo - SP </option>,
-                <option value="Sergipe"> Sergipe - SE </option>,
-                <option value="Tocantins"> Tocantins - TO </option>
-
-              </select><br /><br />
+                <option value="Alagoas" label='Alagoas - (AL)'></option>
+                <option value="Amapá" label='Amapá - (AP)'> </option>,
+                <option value="Amazonas" label='Amazonas - (AM) '> </option>,
+                <option value="Bahia" label='Bahia - (BA)'> </option>,
+                <option value="Ceará" label='Ceará - (CE)'> </option>,
+                <option value="Distrito Federal" label='Distrito Federal - (DF)'></option>,
+                <option value="Espírito Santo" label='Espírito Santo - (ES)'></option>,
+                <option value="Goiás" label='Goiás - (GO)'></option>,
+                <option value="Maranhão" label='Maranhão - (MA)'>  </option>,
+                <option value="Mato Grosso" label='Mato Grosso - (MT)'> </option>,
+                <option value="Mato Grosso do Sul" label='Mato Grosso do Sul - (MS)'></option>,
+                <option value="Minas Gerais" label='Minas Gerais - (MG)'> </option>,
+                <option value="Pará" label='Pará - (PA)'></option>,
+                <option value="Paraíba" label='Paraíba - (PB)'> </option>,
+                <option value="Paraná" label='Paraná - (PR)'></option>,
+                <option value="Pernambuco" label='Pernambuco - (PE)'></option>,
+                <option value="Piauí" label='Piauí - (PI)'></option>,
+                <option value="Rio de Janeiro " label='Rio de Janeiro - (RJ)'> </option>,
+                <option value="Rio Grande do Norte" label='Rio Grande do Norte - (RN)'> </option>,
+                <option value="Rio Grande do Sul" label='Rio Grande do Sul - (RS)'> </option>,
+                <option value="Rondônia" label='Rondônia - (RO)'> </option>,
+                <option value="Roraima " label='Roraima - (RR)'>  </option>,
+                <option value="Santa Catarina" label='Santa Catarina - (SC)'>  </option>,
+                <option value="São Paulo" label='São Paulo - (SP)'>  </option>,
+                <option value="Sergipe" label='Sergipe - (SE) '> </option>,
+                <option value="Tocantins" label='Tocantins - (TO) '> </option>
+              </select>
+              <label for="eS">Selecione o Estado de Destino</label><br /><br />
             </div>
           </div>
         </div>
-        <button class="btn btn-primary" type="submit">
+        <button class="btn btn-primary btn-lg mb-3"  type="submit">
           Enviar
-          </button>
-        <div>
-        <label>
-          <p>Resultado da Pesquisa</p>
-        <textarea name='respostas' id='resposta' disabled rows={30} cols={100}/>
-        </label>
+        </button>
+        <br /><br />
+        <div className="form">
+            <textarea class="form-control form-control-sm mb-3" name='respostas' id='resposta' disabled rows={10} cols={0} ></textarea>
+            <label for="respostas">Resposta gerada pelo ChatGPT</label>
         </div>
       </form>
     </div>
